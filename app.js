e260d1b9f5165437a8ca200574603e3a9fb57c25
trananquan <130271615+trananquan@users.noreply.github.com>
2024-10-13 18:47:50 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const url = `https://yourappname.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
